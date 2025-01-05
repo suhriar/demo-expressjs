41242d3e81995638c74bce513c06e3e315af366b
suhriar <mochamad.suhri@gmail.com>
2025-01-06 01:59:23 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const express = require('express')
+
+const PORT = 8000
+const HOST = '0.0.0.0'
+
+const app = express()
+/*
+[ROUTE] '/'
+[Response] => Hello world!
+ */
+app.get('/', (req, res) => {
+    res.send('Hello world!\n')
+})
+
+app.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
